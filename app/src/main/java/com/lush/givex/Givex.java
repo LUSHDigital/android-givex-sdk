321c166ac28e58dfb51e03d62c512fac917275ec
Matt Allen <matthew.allen@lush.co.uk>
2017-04-26 10:31:21 +0100

    Implemented the activate card request

--- app/src/main/java/com/lush/givex/Givex.java
+++ app/src/main/java/com/lush/givex/Givex.java
@@ -2,8 +2,13 @@ package com.lush.givex;
 
 import android.content.Context;
 
+import com.android.volley.Request;
 import com.android.volley.RequestQueue;
+import com.android.volley.Response;
 import com.android.volley.toolbox.Volley;
+import com.lush.givex.model.request.ActivateCardRequestData;
+import com.lush.givex.model.response.ActivateCardResponse;
+import com.lush.givex.request.ActivateCardRequest;
 
 /**
  * The top-level interface for all actions to be done against the Givex service
@@ -30,8 +35,26 @@ public class Givex
 		this.languageCode = languageCode;
 	}
 
-	public void activateCard(String cardNumber)
+	/**
+	 * Some requests require a transaction code to be a part of the request.
+	 *
+	 * @return Generated transaction code based on environment variables.
+	 */
+	public String createTransactionCode()
 	{
+		return languageCode + "-" + String.valueOf(System.currentTimeMillis());
+	}
 
+	public String activateCard(String cardNumber, double amount, Response.Listener<ActivateCardResponse> listener, Response.ErrorListener errorListener)
+	{
+		return activateCard(cardNumber, amount, createTransactionCode(), listener, errorListener);
+	}
+
+	public String activateCard(String cardNumber, double amount, String transactionCode, Response.Listener<ActivateCardResponse> listener, Response.ErrorListener errorListener)
+	{
+		ActivateCardRequestData data = new ActivateCardRequestData(username, password, languageCode, transactionCode, amount, cardNumber);
+		Request request = new ActivateCardRequest(environment, Request.Method.POST, data, listener, errorListener);
+		queue.add(request);
+		return transactionCode;
 	}
 }
