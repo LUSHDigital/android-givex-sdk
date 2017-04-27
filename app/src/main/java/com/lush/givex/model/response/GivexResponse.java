b90d5f3983392dc1fb0a17eeac161c50cbdbc065
Matt Allen <matthew.allen@lush.co.uk>
2017-04-27 15:27:04 +0100

    Changed the way that responses are parsed from the givex service

--- app/src/main/java/com/lush/givex/model/response/GivexResponse.java
+++ app/src/main/java/com/lush/givex/model/response/GivexResponse.java
@@ -1,9 +1,29 @@
 package com.lush.givex.model.response;
 
+import com.google.gson.Gson;
+import com.lush.givex.model.JsonRpc;
+
+import java.util.List;
+
 /**
  * @author Matt Allen
  */
 public abstract class GivexResponse
 {
-	public abstract void fromNetworkResponse(String response);
+	public void fromNetworkResponse(String response)
+	{
+		Gson gson = new Gson();
+		JsonRpc json = gson.fromJson(response, JsonRpc.class);
+		if (json.getResult() != null)
+		{
+			parseResult(json.getResult());
+		}
+		if (json.getError() != null)
+		{
+			parseError(json.getError());
+		}
+	}
+
+	public abstract void parseResult(List<String> result);
+	public abstract void parseError(List<String> error);
 }
