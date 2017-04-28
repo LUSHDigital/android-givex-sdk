adbeb01dc32bc5fdc933d362e249edd1043e18fc
Matt Allen <matthew.allen@lush.co.uk>
2017-04-28 16:15:03 +0100

    Implemented the responses to actions available on the givex service

--- app/src/main/java/com/lush/givex/model/response/ActivateCardResponse.java
+++ app/src/main/java/com/lush/givex/model/response/ActivateCardResponse.java
@@ -1,27 +1,72 @@
 package com.lush.givex.model.response;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
 /**
+ * Possible error codes are stored here as they appear:
+ *
+ * <ul>
+ *     <li>Code 8: Card already activated</li>
+ *     <li>Code 12: Incorrect amount</li>
+ * </ul>
+ *
  * @author Matt Allen
  */
 public class ActivateCardResponse extends GivexResponse
 {
-	private String code, transactionCode, error;
+	private String transactionCode, error, transactionReference, receiptMessage;
 	private int result;
-	private double cardBalance;
-	private Date cardExpiration;
+	private double balance;
+	private Date expirationDate;
+	private boolean success;
 
 	@Override
 	public void parseResult(List<String> result)
 	{
+		transactionCode = result.get(0);
+		this.result = Integer.parseInt(result.get(1));
+		SimpleDateFormat sdf = new SimpleDateFormat("", Locale.US); // TODO Date format
 		switch (result.size())
 		{
-			case 7:
-				// Success
+			case 5:
+				transactionReference = result.get(2);
+				balance = Double.parseDouble(result.get(3));
+				try
+				{
+					expirationDate = sdf.parse(result.get(4));
+				}
+				catch (ParseException e)
+				{
+					e.printStackTrace();
+				}
+				success = true;
+				break;
 
-			case 5
+			case 6:
+				transactionReference = result.get(2);
+				balance = Double.parseDouble(result.get(3));
+				try
+				{
+					expirationDate = sdf.parse(result.get(4));
+				}
+				catch (ParseException e)
+				{
+					e.printStackTrace();
+				}
+				receiptMessage = result.get(5);
+				success = true;
+				break;
+
+			case 3:
+				error = result.get(2);
+				break;
+
+			default:
+				break;
 		}
 	}
 
@@ -29,11 +74,6 @@ public class ActivateCardResponse extends GivexResponse
 	public void parseError(List<String> error)
 	{
 
-	}
-
-	public String getCode()
-	{
-		return code;
 	}
 
 	public String getTransactionCode()
@@ -51,13 +91,28 @@ public class ActivateCardResponse extends GivexResponse
 		return result;
 	}
 
-	public double getCardBalance()
+	public double getBalance()
 	{
-		return cardBalance;
+		return balance;
 	}
 
-	public Date getCardExpiration()
+	public Date getExpirationDate()
 	{
-		return cardExpiration;
+		return expirationDate;
+	}
+
+	public String getTransactionReference()
+	{
+		return transactionReference;
+	}
+
+	public String getReceiptMessage()
+	{
+		return receiptMessage;
+	}
+
+	public boolean isSuccess()
+	{
+		return success;
 	}
 }
--- app/src/main/java/com/lush/givex/model/response/CancelCardResponse.java
+++ app/src/main/java/com/lush/givex/model/response/CancelCardResponse.java
@@ -1,13 +1,117 @@
 package com.lush.givex.model.response;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+
 /**
+ * Known result codes:
+ *
+ * <ul>
+ *     <li>Code 15: Transaction too old</li>
+ * </ul>
+ *
  * @author Matt Allen
  */
 public class CancelCardResponse extends GivexResponse
 {
-	@Override
-	public void fromNetworkResponse(String response)
-	{
+	private String transactionCode, transactionReference, error, receiptMessage;
+	private int result;
+	private double remainingBalance;
+	private Date expirationDate;
+	private boolean success = false;
 
+	@Override
+	public void parseResult(List<String> result)
+	{
+		transactionCode = result.get(0);
+		this.result = Integer.parseInt(result.get(1));
+		SimpleDateFormat sdf = new SimpleDateFormat("", Locale.US); // TODO Date format
+		switch (result.size())
+		{
+			case 5:
+				transactionReference = result.get(2);
+				remainingBalance = Double.parseDouble(result.get(3));
+				try
+				{
+					expirationDate = sdf.parse(result.get(4));
+				}
+				catch (ParseException e)
+				{
+					e.printStackTrace();
+				}
+				success = true;
+				break;
+
+			case 6:
+				transactionReference = result.get(2);
+				remainingBalance = Double.parseDouble(result.get(3));
+				try
+				{
+					expirationDate = sdf.parse(result.get(4));
+				}
+				catch (ParseException e)
+				{
+					e.printStackTrace();
+				}
+				receiptMessage = result.get(5);
+				success = true;
+				break;
+
+			case 3:
+				error = result.get(2);
+				break;
+
+			default:
+				break;
+		}
+	}
+
+	@Override
+	public void parseError(List<String> error)
+	{
+		// TODO
+	}
+
+	public String getTransactionCode()
+	{
+		return transactionCode;
+	}
+
+	public String getTransactionReference()
+	{
+		return transactionReference;
+	}
+
+	public String getError()
+	{
+		return error;
+	}
+
+	public String getReceiptMessage()
+	{
+		return receiptMessage;
+	}
+
+	public int getResult()
+	{
+		return result;
+	}
+
+	public double getRemainingBalance()
+	{
+		return remainingBalance;
+	}
+
+	public Date getExpirationDate()
+	{
+		return expirationDate;
+	}
+
+	public boolean isSuccess()
+	{
+		return success;
 	}
 }
