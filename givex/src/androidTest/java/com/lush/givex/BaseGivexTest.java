316e4f348c2b9c86650edfeae7cb36e7d800fa04
Matt Allen <matthew.allen@lush.co.uk>
2017-08-08 11:48:10 +0100

    Updated the base givex test with the new constructor

--- givex/src/androidTest/java/com/lush/givex/BaseGivexTest.java
+++ givex/src/androidTest/java/com/lush/givex/BaseGivexTest.java
@@ -32,6 +32,6 @@ public abstract class BaseGivexTest
 	@Before
 	public void setUp() throws Exception
 	{
-		givex = new Givex(InstrumentationRegistry.getTargetContext(), "INSERT_USER_HERE", "INSERT_PASSWORD_HERE", Environment.TEST, "en");
+		givex = new Givex(InstrumentationRegistry.getTargetContext(), "INSERT_USER_HERE", "INSERT_PASSWORD_HERE", "https://dev-dataconnect.givex.com", "en");
 	}
 }
